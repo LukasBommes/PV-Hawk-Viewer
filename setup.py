85c249e5765f5b730aa49cb8bd6a20e6d6639d8f
LukasBommes <lukas.bommes@gmx.de>
2021-12-08 19:22:06 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="PV-Drone-Inspect-Viewer",
     maintainer="Lukas Bommes",
     url='https://github.com/LukasBommes/PV-Drone-Inspect-Viewer',
-    version="1.0.1", 
+    version="1.0.0", 
     packages=find_packages(),   
     package_dir={name: name},    
     include_package_data=True,
